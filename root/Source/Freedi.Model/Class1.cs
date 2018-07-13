8d42c3571a14bcd8fe66afd776892fc711b212e4
Андрей Зенько <andrey_zianko@outlook.com>
2018-07-13 14:19:30 +0300

    s

---
+++ root/Source/Freedi.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freedi.Model
+{
+    public class Class1
+    {
+    }
+}
